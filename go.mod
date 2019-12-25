0dbe8dcbb4e46639a74f03fb36767eebea9afdd7
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-25 18:37:54 -0500

    reverting for ease of reading

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/reserviord/stdin
-
-go 1.13
-
-require github.com/reservoird/ibool v1.0.0
--- stdin.go
+++ stdin.go
@@ -2,72 +2,27 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
-	"time"
-
-	"github.com/reservoird/ibool"
 )
 
-// needed to aid in unit testing
-type ireader interface {
-	ReadString(byte) (string, error)
-}
-
-type boolbridge struct {
-}
-
-func (o *boolbridge) Val() bool {
-	return true
-}
-
 type stdin struct {
-	Timestamp   bool
-	reader      ireader
-	keepRunning ibool.IBool
 }
 
 // Config configures ingester
 func (o *stdin) Config(cfg string) error {
-	// default
-	o.Timestamp = false
-	o.reader = bufio.NewReader(os.Stdin)
-	o.keepRunning = &boolbridge{}
-
-	if cfg != "" {
-		b, err := ioutil.ReadFile(cfg)
-		if err != nil {
-			return err
-		}
-		s := stdin{}
-		err = json.Unmarshal(b, &s)
-		if err != nil {
-			return err
-		}
-		o.Timestamp = s.Timestamp
-	}
 	return nil
 }
 
 // Ingest reads data from stdin and writes it to a channel
 func (o *stdin) Ingest(channel chan<- []byte) error {
-	for o.keepRunning.Val() == true {
-		line, err := o.reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
 		if err != nil {
-			if err != io.EOF {
-				return err
-			}
+			return err
 		}
-		if o.Timestamp == true {
-			channel <- []byte(fmt.Sprintf("rditime %s %s", time.Now().Format(time.RFC3339), line))
-		} else {
-			channel <- []byte(line)
-		}
+		channel <- []byte(line)
 	}
-	return nil
 }
 
 // Ingester for stdin

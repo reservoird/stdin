3a7f56ac8301d00fd659e48616d84ff7a72696ce
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-25 20:23:44 -0500

    updating to end gracefully

--- stdin.go
+++ stdin.go
@@ -3,26 +3,39 @@ package main
 import (
 	"bufio"
 	"os"
+	"sync"
 )
 
 type stdin struct {
+	run  bool
+	done <-chan struct{}
+	wg   *sync.WaitGroup
 }
 
 // Config configures ingester
-func (o *stdin) Config(cfg string) error {
+func (o *stdin) Config(cfg string, done <-chan struct{}, wg *sync.WaitGroup) error {
+	o.done = done
+	o.wg = wg
 	return nil
 }
 
 // Ingest reads data from stdin and writes it to a channel
 func (o *stdin) Ingest(channel chan<- []byte) error {
 	reader := bufio.NewReader(os.Stdin)
-	for {
+	for o.run == true {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			return err
 		}
 		channel <- []byte(line)
+
+		select {
+		case <-o.done:
+			o.run = false
+		default:
+		}
 	}
+	return nil
 }
 
 // Ingester for stdin

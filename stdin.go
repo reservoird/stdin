6a6fc63c44d57c89750d052593bd9b49a96c9ef1
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-27 21:31:36 -0500

    updating to add in timestamps and name

--- stdin.go
+++ stdin.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/reservoird/icd"
 )
 
 type stdin struct {
-	run bool
+	run       bool
+	Name      string
+	Timestamp bool
 }
 
 // NewIngester is what reservoird to create and start stdin
@@ -19,6 +25,17 @@ func NewIngester() (icd.Ingester, error) {
 
 // Config configures ingester
 func (o *stdin) Config(cfg string) error {
+	d, err := ioutil.ReadFile(cfg)
+	if err != nil {
+		return err
+	}
+	s := stdin{}
+	err = json.Unmarshal(d, &s)
+	if err != nil {
+		return err
+	}
+	o.Name = s.Name
+	o.Timestamp = s.Timestamp
 	return nil
 }
 
@@ -34,6 +51,9 @@ func (o *stdin) Ingest(queue icd.Queue, done <-chan struct{}, wg *sync.WaitGroup
 			return err
 		}
 		if line != "" {
+			if o.Timestamp == true {
+				line = fmt.Sprintf("%s %s:", o.Name, time.Now().Format(time.RFC3339)) + line
+			}
 			err = queue.Push([]byte(line))
 			if err != nil {
 				return err

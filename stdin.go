92dad3275f5d47ac5ca3b33bed1cacb4b5d2e387
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-29 17:32:52 -0500

    adding in statistics generation

--- stdin.go
+++ stdin.go
@@ -12,19 +12,26 @@ import (
 	"github.com/reservoird/icd"
 )
 
-type stdin struct {
-	stats     chan<- string
-	run       bool
-	Tag       string
+type stdinCfg struct {
+	Name      string
 	Timestamp bool
 }
 
-// New is what reservoird to create and start stdin
-func New(cfg string, stats chan<- string) (icd.Ingester, error) {
-	o := &stdin{
-		stats:     stats,
-		run:       true,
-		Tag:       "stdin",
+type stdinStats struct {
+	MsgsRead uint64
+	Active   bool
+}
+
+type stdin struct {
+	cfg       stdinCfg
+	stats     stdinStats
+	statsChan chan<- string
+}
+
+// New is what reservoird uses to create and start stdin
+func New(cfg string, statsChan chan<- string) (icd.Ingester, error) {
+	c := stdinCfg{
+		Name:      "stdin",
 		Timestamp: false,
 	}
 	if cfg != "" {
@@ -32,47 +39,65 @@ func New(cfg string, stats chan<- string) (icd.Ingester, error) {
 		if err != nil {
 			return nil, err
 		}
-		err = json.Unmarshal(d, o)
+		err = json.Unmarshal(d, &c)
 		if err != nil {
 			return nil, err
 		}
+	}
+	o := &stdin{
+		cfg:       c,
+		stats:     stdinStats{},
+		statsChan: statsChan,
 	}
 	return o, nil
 }
 
 // Name return the name of the Ingester
 func (o *stdin) Name() string {
-	return o.Tag
+	return o.cfg.Name
 }
 
-// Ingest reads data from stdin and writes it to a channel
+// Ingest reads data from stdin and writes it to the queue
 func (o *stdin) Ingest(queue icd.Queue, done <-chan struct{}, wg *sync.WaitGroup) error {
-	defer wg.Done()
+	defer wg.Done() // required
 
 	reader := bufio.NewReader(os.Stdin)
-	o.run = true
-	for o.run == true {
+	o.stats.Active = true
+	for o.stats.Active == true {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Printf("%v\n", err)
 		} else {
-			if line != "" {
-				if o.Timestamp == true {
-					line = fmt.Sprintf("[%s %s] ", o.Name(), time.Now().Format(time.RFC3339)) + line
-				}
-				err = queue.Put([]byte(line))
-				if err != nil {
-					fmt.Printf("%v\n", err)
+			o.stats.MsgsRead = o.stats.MsgsRead + 1
+			if queue.Closed() == false {
+				if len(line) != 0 {
+					if o.cfg.Timestamp == true {
+						line = fmt.Sprintf("[%s %s] ", o.Name(), time.Now().Format(time.RFC3339)) + line
+					}
+					err = queue.Put([]byte(line))
+					if err != nil {
+						fmt.Printf("%v\n", err)
+					}
 				}
 			}
 		}
 
 		select {
 		case <-done:
-			o.run = false
+			o.stats.Active = false
 		default:
-			time.Sleep(time.Millisecond)
 		}
+
+		stats, err := json.Marshal(o.stats)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		select {
+		case o.statsChan <- string(stats):
+		default:
+		}
+
+		time.Sleep(time.Millisecond)
 	}
 	return nil
 }
